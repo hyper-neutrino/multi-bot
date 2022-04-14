37c9b5b33819986630de29cd7b91a5b8979d7a0f
hyper-neutrino <alex.yj.liao@gmail.com>
2022-04-14 18:04:44 -0400

    add settings list command

---
+++ src/commands/list-settings.js
@@ -0,0 +1,20 @@
+import { Command } from "paimon.js";
+import db from "../db.js";
+
+export const command = new Command({
+    name: "list-settings",
+    description: "List all currently defined settings.",
+    options: [],
+    async execute(_) {
+        const rows = [];
+
+        for (const entry of await db.settings.find({}).toArray()) {
+            if (entry.value !== undefined) {
+                rows.push(`${entry.key} = ${entry.value}`);
+            }
+        }
+
+        return rows.join("\n") || "(none)";
+    },
+    permission: "setting",
+});
