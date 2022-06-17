0b6890ddf7a652da91cc7b0635ea36604690fd1a
hyper-neutrino <alex.yj.liao@gmail.com>
2022-06-17 11:17:09 -0400

    bruh

--- src/lib/message-logs.js
+++ src/lib/message-logs.js
@@ -31,25 +31,15 @@ export async function is_logger_ignoring(channel) {
 }
 
 export async function is_loggable(message, skip_channel, ignore_skip) {
-    console.log("1");
     if (!ignore_skip && purge_log_skip.has(message.id)) return false;
-    console.log("2");
     if (message.content === null) return false;
-    console.log("3");
     if (!message.guild) return false;
-    console.log("4");
     if (message.system) return false;
-    console.log("5");
     if (message.guild.id != client.home.id) return false;
-    console.log("6");
     if (message.webhookId) return false;
-    console.log("7");
     if (message.author?.bot) return false;
-    console.log("8");
     if (skip_channel) return true;
-    console.log("9");
     if (await is_logger_ignoring(message.channel)) return false;
-    console.log("10");
 
-    return false;
+    return true;
 }
